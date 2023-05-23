bdde73cc097b45fb5f23841872609532275205a0
NBadeloe <48018083+NBadeloe@users.noreply.github.com>
2023-05-23 21:48:20 +0200

    changed name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,11 @@
   import GlitterCarousel from "../lib/components/GlitterCarousel.svelte";
 import Glittercarousel from "../lib/components/GlitterCarousel.svelte";
   import Layout from "../lib/components/Layout.svelte";
-  import Zigzag from "../lib/components/Zigzag.svelte";
+  import Albums from "../lib/components/Albums.svelte";
 
 </script>
 <!-- <Layout/> -->
 <!-- <GlitterCarousel/> -->
-<Zigzag/>
+<Albums/>
 
 
